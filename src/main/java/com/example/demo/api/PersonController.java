11fed3966f7812bf2ea58b100d2a8fab78f8acb8
teukurijal <tmrijalul@gmail.com>
2019-12-30 16:12:31 +0700

    simple add person

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,24 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/vi/person")
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+}

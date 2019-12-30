bed8df75847a3c148a8beda65f0fc231a1cc7eec
teukurijal <tmrijalul@gmail.com>
2019-12-30 17:12:09 +0700

    add delete

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,11 +3,12 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/vi/person")
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -18,7 +19,18 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id)
+                .orElse(null);
     }
 }
--- src/main/java/com/example/demo/dao/FakePersonDataAccsessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccsessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -17,4 +18,32 @@ public class FakePersonDataAccsessService implements PersonDao {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if (personMaybe.) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePerson(UUID id, Person person) {
+        return 0;
+    }
+
 }
